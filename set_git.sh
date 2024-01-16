a73463823cc9a528609f63f5a1213affcfaab6d9
nabimari <65827756+nabimari@users.noreply.github.com>
2024-01-16 08:47:51 +0200

    first commit

--- set_git.sh
+++ set_git.sh
@@ -13,5 +13,5 @@
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin git@github.com:nabimari/braude-web-1.git
+git remote add origin https://github.com/nabimari/braude-web-1.git
 git push -u origin main
